b727c11ac430b0fd6af3767330fcef6fb210d48f
chamered <smaltauro@unibz.it>
2025-05-06 13:49:57 +0200

    Added PieceColor enum

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -1,14 +1,21 @@
 package pieces;
 
 public abstract class Piece {
+    public enum PieceColor {BLACK, WHITE}
+
     int value;
-    public Piece(int value){
+    PieceColor color;
+
+    public Piece(int value, PieceColor color){
         this.value = value;
+        this.color = color;
     }
+
     /**
      *  Moves the piece according to it's capabilities
      */
     abstract public void move();
+
     /**
      * @param piece The piece that's being allegedly eaten
      * @return true iff this Piece can eat the other piece
