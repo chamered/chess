718414a88cc3e357f411ad77827b779a51348a26
KhushikaGupta <KGupta@unibz.it>
2025-06-20 07:56:22 +0200

    generatePossibelMoves

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -1,5 +1,13 @@
 package pieces;
 
+
+import java.util.List;
+
+import board.ChessBoard;
+import board.Move;     // This is a class that holds information about a move
+import board.Position; // This is a class that tells where a piece is on the board
+
+
 public abstract class Piece {
     public enum PieceColor {BLACK, WHITE}
 
@@ -21,4 +29,13 @@ public abstract class Piece {
      * @return true iff this Piece can eat the other piece
      */
     abstract public boolean eatOtherPiece(Piece piece);
+
+/**
+ * Find all possible moves this piece can make from its current position
+ *
+ * @param board The current game board (so we know which squares are free or taken)
+ * @param currentPos Where this piece is right now on the board
+ * @return A list of all possible moves this piece can make
+ */
+abstract public List<Move> generatePossibleMoves(ChessBoard board, Position currentPos);
 }
