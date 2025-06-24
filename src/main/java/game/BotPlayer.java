613f8209b9e7fbb96d27c0a3d70b524c7c6c76a3
NikDev <nzappa@unibz.it>
2025-06-24 10:33:44 +0200

    Implemented boardEvaluation for minimax algorithm

--- src/main/java/game/BotPlayer.java
+++ src/main/java/game/BotPlayer.java
@@ -2,6 +2,7 @@ package game;
 
 import board.BoardImpl;
 import pieces.Color;
+import pieces.Piece;
 
 public class BotPlayer extends Player{
 
@@ -17,7 +18,32 @@ public class BotPlayer extends Player{
         return 0;
     }
 
+    /**
+     * This method uses the value of the pieces to do a total evaluation
+     * of all the pieces inside the current board
+     * @param board the current board
+     * @param botColor the color of the bot pieces (WHITE or BLACK)
+     * @return The total score of the board
+     */
     public int evaluateBoard(BoardImpl board, Color botColor) {
-        return 0;
+        Piece[][] boardState = board.getBoard();
+        int score = 0;
+
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                Piece piece = boardState[row][col];
+
+                if (piece != null){
+                    //The value of pieces is positive and added if it is the sam color of the BotPlayer
+                    if (piece.getColor() == botColor) score += Math.abs(piece.getValue());
+                    //Is subtracted if it is a different color
+                    else {
+                        score -= Math.abs(piece.getValue());
+                    }
+                }
+            }
+        }
+
+        return score;
     }
 }
